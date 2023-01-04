6d847e4f82506e2ad77e1d3ef6a99619ce282757
Claus <matheuscdd@hotmail.com>
2023-01-04 12:07:11 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components'
+import { Reset } from './reset';
+
+export const GlobalStyle = createGlobalStyle`
+    ${Reset};
+    
+`
