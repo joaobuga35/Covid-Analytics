c86c4e08d0d065fc7a76d3debfd081ed9ccc380c
joaobuga35 <joaobuga35@gmail.com>
2023-01-04 15:10:19 -0300

    feat: reset and global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,37 @@
-import { createGlobalStyle } from 'styled-components'
-import { Reset } from './reset';
+import { createGlobalStyle } from "styled-components";
+import { Reset } from "./reset";
 
 export const GlobalStyle = createGlobalStyle`
-    ${Reset};
+ ${Reset};
+:root {
+    --white: #FFFFFF;
+    --gray-1: #F7F7F7;
+    --gray-2: #888888;
+    --gray-3: #6B6B6B;
+    --gray-4:#656565;
+    --gray-5:#444444;
+    --gray-trans:rgba(217, 217, 217, 0.5)
+    --color-blue: #0083D1;
+    --black:#000;
+    --color-error: #E60000; 
+    --color-success: #168821;
+    --color-warning: #FFCD07;
+
+    --font-family-1: 'Inter', sans-serif;
+    --font-family-2: 'Roboto', sans-serif;
+    --font-family-3: 'Dhurjati', sans-serif;
+
+    --title-header:70px;
+    --title-1: 40px;
+    --title-2: 30px;
+    --name-states:12px;
+    --label:14px;
+    --font-button:30px;
+
+    --radius-1:2px;
+    --radius-2:5px;
+    --radius-3: 30px;
     
-`
+  }
+    
+`;
