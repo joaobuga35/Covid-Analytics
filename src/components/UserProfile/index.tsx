fff3f35139e857e30d77f89b064dd799f2a75728
Lucas Araujo <lucas984618740@gmail.com>
2023-01-10 10:54:32 -0300

    img defalt

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -12,8 +12,17 @@ export function UserProfile() {
   return (
     <SectionStyle>
       <UserStyle>
+
+
         <div>
-          <img src={logedUser.img} alt={logedUser.name} title={logedUser.name} />
+
+          {logedUser.img?
+
+           <img  src={logedUser.img} onError={({ currentTarget }) => {
+                currentTarget.onerror = null; 
+                currentTarget.src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+              }} alt={logedUser.name} title={logedUser.name} /> :  <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/> }
+
         </div>
         <h2>{logedUser.name}</h2>
       </UserStyle>
